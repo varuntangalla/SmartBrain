486dc23048dbd059a2a9bb99d6db3216a5732974
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-09-23 23:04:15 -0700

    First Commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+
+
+
+
+const Rank = () => {
+
+	return ( 
+
+		<div>
+
+		<div>
+
+		<div className='white f3'>
+
+			{'Varun Tangalla, Your current Rank is...'}
+
+			<div className='white f1'>
+
+			{'#6'}
+
+		</div>
+
+
+		</div>
+
+		
+        </div>
+
+        </div>
+
+		);
+
+}
+
+
+export default Rank;
